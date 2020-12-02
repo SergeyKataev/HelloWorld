4ac3d0cf2918f5bcbac3257c25af247098122fa2
sergeykataev <sergeykataev1992@gmail.com>
2020-12-02 11:58:10 +0300

    traffic light

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -9,10 +9,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var redSign: UIButton!
+    @IBOutlet weak var yellowSign: UIButton!
+    @IBOutlet weak var greenSign: UIButton!
+    @IBOutlet weak var startButton: UIButton!
+
+    var currentNumColor = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redSign.layer.cornerRadius = 0.5 * redSign.bounds.size.width
+        yellowSign.layer.cornerRadius = 0.5 * yellowSign.bounds.size.width
+        greenSign.layer.cornerRadius = 0.5 * greenSign.bounds.size.width
+        
+        startButton.layer.cornerRadius = 5
+        redSign.alpha = 0.5
+        yellowSign.alpha = 0.5
+        greenSign.alpha = 0.5
     }
+    
+    @IBAction func startButtonPressed(_ sender: UIButton) {
+        startButton.setTitle("Next", for: .normal)
+        
+        redSign.alpha = 0.5
+        yellowSign.alpha = 0.5
+        greenSign.alpha = 0.5
+        
+        currentNumColor += 1
+        if currentNumColor > 3 {
+            currentNumColor = 1
+        }
+        switch currentNumColor {
+        case 1:
+            redSign.alpha = 1
+        case 2:
+            yellowSign.alpha = 1
+        case 3:
+            greenSign.alpha = 1
+        default:
+            print("some error")
+        }
+        
+        
+    }
+    
 
 
 }
